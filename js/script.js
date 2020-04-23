f5b5492599662d0a51e5a46802fdf07f28740f90
Adeel Saleem <62308816+adeelsaleem-se@users.noreply.github.com>
2020-04-23 15:25:33 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+var helloSpeaker =  {
+
+  speakWord : "Hello",
+
+  speak :function (name) {
+    console.log(this.speakWord + " " + name);
+  }
+}
+
+
+var byeSpeaker =  {
+
+   speakWord : "Good Bye",
+
+   speak: function (name) {
+      console.log(this.speakWord + " " + name);
+  }
+}
+
+
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  
+  for (var i = 0; i < names.length; i++) {
+  
+    var firstLetter = names[i].charAt(0).toLowerCase();
+  
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
